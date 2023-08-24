fc361806c39ce3d09018db6c136a4d5f602f6ce7
fana18 <fanaa1827@gmail.com>
2023-08-24 20:35:12 -0300

    pre-entrega 3

--- main.js
+++ main.js
@@ -1,76 +1,72 @@
-/* 
-PRE-ENTREGA2: cosas a incluir 
-arrays
-objetos 
-funciones de alto ordeb 
-metodos 
-van los temas de la clase 4 hasta la clase 8
-*/
-let saludo = alert(`Bienvenido/a a nuestra tienda `);
-let pedirNombre = prompt(`Ingrese su nombre de usuario porfavor `);
-let carrito = [
-  { nombre: "Camisa", precio: 12000, id: 1 },
-  { nombre: "Jean", precio: 12500, id: 2 },
-  { nombre: "Remera Polo", precio: 12700, id: 3 },
-  { nombre: "Pantalon Cargo", precio: 13500, id: 4 },
-  { nombre: "Campera", precio: 21000, id: 5 },
-  { nombre: "Buzo", precio: 14000, id: 6 },
-  { nombre: "Sweater", precio: 12300, id: 7 },
-  { nombre: "Cardigan", precio: 12900, id: 8 },
-];
+const divproductos = document.getElementById("productos");
+const divcarrito = document.getElementById("carrito");
+const divtotal = document.getElementById("total");
 
-// tengo que mostrar los productos que estan en el arreglo
-let productosDisponibles = "Productos disponibles:\n";
-for (let producto of carrito) {
-  productosDisponibles += `${producto.nombre}\n`;
-}
-alert(productosDisponibles);
+let carrito = [];
 
-//  compra
-let productosElegidos = prompt(
-  "Ingrese los nombres de los productos que desea comprar, separados por comas:"
-).toLowerCase();
+document.addEventListener("DOMContentLoaded", () => {
+  let productosdisponibles = JSON.parse(localStorage.getItem("productos"));
+  generartarjetas(productosdisponibles);
+});
 
-//productos
-let productosSeleccionados = [];
-if (productosElegidos) {
-  let productosElegidosArray = productosElegidos.split(",");
-  for (let nombreProducto of productosElegidosArray) {
-    let productoEncontrado = carrito.find(
-      (producto) =>
-        producto.nombre.toLowerCase() === nombreProducto.trim().toLowerCase()
-    );
-    if (productoEncontrado) {
-      productosSeleccionados.push(productoEncontrado);
-    }
-  }
-}
+const generartarjetas = (productos) => {
+  productos.forEach((producto) => {
+    let card = document.createElement("div");
+    card.className = "producto";
+    card.innerHTML = `
+      <div class="card col-lg-4 col-md-4 col-sm-12 p-5px" style="width: 18rem;">
+        <img src="${producto.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <p class="card-text">${producto.categoria}</p>
+          <p class="card-text">${producto.precio}</p>
+          <button id="btncomprar${producto.id}" class="btn btn-primary">Comprar</button>
+        </div>
+      </div>`;
 
-// Mostrar nombres
-let detallesProductos = "Detalles de productos seleccionados:\n";
-let total = productosSeleccionados.reduce((acumulador, producto) => {
-  detallesProductos += ` ${producto.nombre} - Precio: ${producto.precio}\n`;
-  return acumulador + producto.precio;
-}, 0);
+    divproductos.appendChild(card);
 
-alert(detallesProductos);
+    const btncomprar = document.getElementById(`btncomprar${producto.id}`);
+    btncomprar.addEventListener("click", () => agregarAlCarrito(producto));
+  });
+};
 
-// parte en la que le pregunto al usuario si tiene un descuento
-let descuento = prompt(
-  "¿Tiene un descuento? (escriba  si o  no)"
-).toLowerCase();
-if (descuento === "si") {
-  let porcentajeDescuento = parseFloat(
-    prompt("Ingrese el porcentaje de descuento:")
+const agregarAlCarrito = (producto) => {
+  carrito.push(producto);
+  actualizarCarrito();
+};
+
+const actualizarCarrito = () => {
+  divcarrito.innerHTML = "";
+  carrito.forEach((producto) => {
+    const item = document.createElement("div");
+    item.className = "carrito-item";
+    item.innerHTML = `
+      <p>${producto.nombre} - ${producto.precio}</p>
+      <button class="btn btn-danger btn-sm" id="btneliminar${producto.id}">Eliminar</button>
+    `;
+    divcarrito.appendChild(item);
+
+    const btneliminar = document.getElementById(`btneliminar${producto.id}`);
+    btneliminar.addEventListener("click", () => eliminarDelCarrito(producto));
+  });
+};
+
+const eliminarDelCarrito = (producto) => {
+  carrito = carrito.filter((p) => p.id !== producto.id);
+  actualizarCarrito();
+};
+
+document.getElementById("btnVaciarCarrito").addEventListener("click", () => {
+  carrito = [];
+  actualizarCarrito();
+});
+
+document.getElementById("btnTotal").addEventListener("click", () => {
+  const total = carrito.reduce(
+    (acumulador, producto) => acumulador + producto.precio,
+    0
   );
-  if (porcentajeDescuento === 15) {
-    let descuentoAplicado = total * (porcentajeDescuento / 100);
-    let totalConDescuento = total - descuentoAplicado;
-    alert(`Total con descuento: ${totalConDescuento}`);
-  } else {
-    alert("Solo aceptamos descuentos del 15%.");
-  }
-} else if (descuento === "no") {
-  alert(`Total a pagar: ${total}`);
-}
-let agradecimiento = alert(`Gracias por su compra ${pedirNombre}`);
+  alert(`Total: ${total}`);
+  alert(`gracias por su compra`);
+});
