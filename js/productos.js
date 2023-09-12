8d2df948b8795d64a767e006e8f07b56e319b032
fana18 <fanaa1827@gmail.com>
2023-09-12 16:20:17 -0300

    Proyecto final

--- js/productos.js
+++ js/productos.js
@@ -1,103 +1,154 @@
 const divcarrito = document.getElementById("carrito");
 const divtotal = document.getElementById("total-precio");
-
-const listado = document.getElementById("productosRow");
+const selectOrdenar = document.getElementById("inputGroupSelect01");
+const productosRow = document.getElementById("productosRow");
 let carrito = [];
+let productosData = [];
 
 const pedirProductos = async () => {
-    try {
-        const response = await fetch("../productos.json");
-        const data = await response.json();
+  try {
+    const response = await fetch("../productos.json");
+    const data = await response.json();
 
-        let contador = 0;
-        let row;
+    const productosEnLocalStorage =
+      JSON.parse(localStorage.getItem("carrito")) || [];
 
-        data.forEach((producto, index) => {
-            if (contador % 4 === 0) {
-                
-                row = document.createElement("div");
-                row.className = "row";
-                listado.appendChild(row);
-            }
+    let contador = 0;
+    let row;
 
-            const div = document.createElement("div");
-            div.className = 'col-3 card';
-            div.style = 'width: 15rem;';
-            div.innerHTML = `
-              <img src='../img/${producto.imagen}' class='card-img-top' alt=''>
-              <div class='card-body'>
-                <h5 class='card-title'>${producto.nombre}</h5>
-                <h6 class='card-title card-precio'>$ ${producto.precio}</h6>
-                <a href='#' class='btn btn-primary card-boton' onclick='agregarAlCarrito(event, ${JSON.stringify(producto)})'>Añadir al carrito</a>
-              </div>`;
-
-            row.appendChild(div);
-            contador++;
-        });
-
-    } catch (error) {
-        console.error("Error: " + error);
-    } finally {
-        console.log("Terminado");
-    }
-};
-const agregarAlCarrito = (event, producto) => {
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace #
-    carrito.push(producto);
-    actualizarCarrito();
-  };
-
-  const actualizarCarrito = () => {
-    divcarrito.innerHTML = "";
-    let total = 0;
-
-    carrito.forEach((producto) => {
-      const item = document.createElement("div");
-      item.className = "carrito-item";
-      item.innerHTML = `
-          <p>${producto.nombre} - $${producto.precio}</p>
-          <button class="btn btn-danger btn-sm" onclick='eliminarDelCarrito(${JSON.stringify(
-            producto
-          )})'>Eliminar</button>
-        `;
-      divcarrito.appendChild(item);
-      total += parseFloat(producto.precio);
-    });
-
-    divtotal.textContent = `Total: $${total}`;
-  };
-
-  const eliminarDelCarrito = (producto) => {
-    carrito = carrito.filter((p) => p.id !== producto.id);
-    actualizarCarrito();
-  };
-
-  document
-    .getElementById("btnVaciarCarrito")
-    .addEventListener("click", () => {
-      carrito = [];
-      actualizarCarrito();
-    });
-
-  document.getElementById("btnTotal").addEventListener("click", () => {
-    const total = carrito.reduce(
-      (acumulador, producto) => acumulador + parseFloat(producto.precio),
-      0
-    );
-    Swal.fire({
-      title: `Gracias por su compra`,
-      showClass: {
-        popup: 'animate__animated animate__fadeInDown'
-      },
-      hideClass: {
-        popup: 'animate__animated animate__fadeOutUp'
+    data.forEach((producto, index) => {
+      if (contador % 4 === 0) {
+        row = document.createElement("div");
+        row.className = "row";
+        productosRow.appendChild(row);
       }
-    })
-    
 
-    carrito = [];
-    actualizarCarrito();
+      const div = document.createElement("div");
+      div.className = "col-3 card";
+      div.style = "width: 15rem;";
+      div.innerHTML = `
+        <img src='../img/${producto.imagen}' class='card-img-top' alt=''>
+        <div class='card-body'>
+          <h5 class='card-title'>${producto.nombre}</h5>
+          <h6 class='card-title card-precio'>$ ${producto.precio}</h6>
+          <a href='#' class='btn btn-primary card-boton' onclick='agregarAlCarrito(event, ${JSON.stringify(
+            producto
+          )})'>Añadir al carrito</a>
+        </div>`;
+
+      row.appendChild(div);
+      contador++;
+    });
+
+    if (productosEnLocalStorage.length === 0) {
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+
+    if (productosEnLocalStorage.length > 0) {
+      carrito = productosEnLocalStorage;
+      actualizarCarrito();
+    }
+
+    productosData = data;
+  } catch (error) {
+    console.error("Error: " + error);
+  } finally {
+    console.log("Terminado");
+  }
+};
+
+selectOrdenar.addEventListener("change", () => {
+  const valorSeleccionado = selectOrdenar.value;
+  if (valorSeleccionado === "1") {
+    productosData.sort((a, b) => parseFloat(a.precio) - parseFloat(b.precio));
+  } else if (valorSeleccionado === "2") {
+    productosData.sort((a, b) => parseFloat(b.precio) - parseFloat(a.precio));
+  }
+
+  productosRow.innerHTML = "";
+  carrito = [];
+  productosData.forEach((producto, index) => {
+    const div = document.createElement("div");
+    div.className = "col-3 card";
+    div.style = "width: 15rem;";
+    div.innerHTML = `
+      <img src='../img/${producto.imagen}' class='card-img-top' alt=''>
+      <div class='card-body'>
+        <h5 class='card-title'>${producto.nombre}</h5>
+        <h6 class='card-title card-precio'>$ ${producto.precio}</h6>
+        <a href='#' class='btn btn-primary card-boton' onclick='agregarAlCarrito(event, ${JSON.stringify(
+          producto
+        )})'>Añadir al carrito</a>
+      </div>`;
+
+    productosRow.appendChild(div);
   });
 
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+});
+
+const agregarAlCarrito = (event, producto) => {
+  event.preventDefault();
+  carrito.push(producto);
+  actualizarCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const actualizarCarrito = () => {
+  divcarrito.innerHTML = "";
+  let total = 0;
+
+  carrito.forEach((producto) => {
+    const item = document.createElement("div");
+    item.className = "carrito-item";
+    item.innerHTML = `
+      <p>${producto.nombre} - $${producto.precio}</p>
+      <button class="btn btn-danger btn-sm" onclick='eliminarDelCarrito(${JSON.stringify(
+        producto
+      )})'>Eliminar</button>
+    `;
+    divcarrito.appendChild(item);
+    total += parseFloat(producto.precio);
+  });
+
+  divtotal.textContent = `Total: $${total}`;
+};
+
+const eliminarDelCarrito = (producto) => {
+  carrito = carrito.filter((p) => p.id !== producto.id);
+  actualizarCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+document.getElementById("btnVaciarCarrito").addEventListener("click", () => {
+  carrito = [];
+  actualizarCarrito();
+  localStorage.removeItem("carrito");
+});
+
+document.getElementById("btnTotal").addEventListener("click", () => {
+  const total = carrito.reduce(
+    (acumulador, producto) => acumulador + parseFloat(producto.precio),
+    0
+  );
+  Swal.fire({
+    title: `Gracias por su compra`,
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp",
+    },
+  }).then(() => {
+   
+    window.location.href = "../index.html";
+  });
+
+  localStorage.removeItem("carrito");
+
+  carrito = [];
+  actualizarCarrito();
+});
 
 pedirProductos();
+
